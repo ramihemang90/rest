544151ea1a2460830f338d41cf2a8c06a36c3fac
Hemang Rami <ramihemang90@gmail.com>
2016-05-17 21:22:59 +0530

    Adding configuration properties and UserToken

---
+++ RESTfulExample/src/main/java/in/hemangrami/model/UserToken.java
@@ -0,0 +1,21 @@
+package in.hemangrami.model;
+
+public class UserToken {
+	
+	private String usertoken;
+	
+	public UserToken() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getUsertoken() {
+		return usertoken;
+	}
+
+	public void setUsertoken(String usertoken) {
+		this.usertoken = usertoken;
+	}
+	
+	
+
+}

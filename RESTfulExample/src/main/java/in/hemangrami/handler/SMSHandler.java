ea58aba200182a07ae4b41c03fb69da90a6cd5f3
Hemang Rami <ramihemang90@gmail.com>
2016-05-20 00:03:46 +0530

    Adding support for twilo sms api

--- RESTfulExample/src/main/java/in/hemangrami/handler/SMSHandler.java
+++ RESTfulExample/src/main/java/in/hemangrami/handler/SMSHandler.java
@@ -3,6 +3,14 @@ package in.hemangrami.handler;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.factory.MessageFactory;
+
 /*
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
@@ -15,17 +23,17 @@ import in.hemangrami.model.Message;
 
 public class SMSHandler implements MessageHandler {
 
-	public static final String ACCOUNT_SID = "{{ account_sid }}";
-	public static final String AUTH_TOKEN = "{{ auth_token }}";
+	public static final String ACCOUNT_SID = "ACb689fa1f490e2a9dbd07dc8b82a69d18";
+	public static final String AUTH_TOKEN = "f908bc7fe5b2375b555c54d9f579dc88";
 
 	private String from ="92992929";
 	public SMSHandler(Properties serviceConfig) {
-		
+		from =serviceConfig.getProperty("sms.from");
 	}
 
 	@Override
 	public void deliverMessage(Message message, List<String> receipentList, Properties messageProperties) {
-/*
+
 		TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
 		for (String mobile : receipentList) {
 			// Build a filter for the MessageList
@@ -40,7 +48,7 @@ public class SMSHandler implements MessageHandler {
 			} catch (TwilioRestException e) {
 				System.out.println(e.getErrorMessage());
 			}
-		}*/
+		}
 	}
 
 }
--- RESTfulExample/src/main/java/in/hemangrami/rest/UserService.java
+++ RESTfulExample/src/main/java/in/hemangrami/rest/UserService.java
@@ -1,16 +1,29 @@
 package in.hemangrami.rest;
 
+import java.util.List;
+import java.util.Properties;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import in.hemangrami.db.DBUtils;
+import in.hemangrami.handler.EmailHandler;
+import in.hemangrami.handler.MessageHandler;
+import in.hemangrami.handler.SMSHandler;
+import in.hemangrami.model.Message;
 import in.hemangrami.model.User;
+import in.hemangrami.model.UserToken;
 
-@Path("/user")
+@Path("/service/notify")
 public class UserService extends BaseService {
 
+	private MessageHandler emailHandler = new EmailHandler(getServiceConfig());
+	private MessageHandler smsHandler = new SMSHandler(getServiceConfig());
+	
+	
 	@POST
 	@Path("/login")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -27,21 +40,74 @@ public class UserService extends BaseService {
 		}
 
 	}
+	
+
+	@POST
+	@Path("/sendmsg")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response sendMessage(UserToken message) {
+
+		// Authenticating and Authorizing request
+		if (authorizeUser(message.getUsertoken()) != 200) {
+			return Response.status(403).header("content-type", "application/json").entity("{\"message\":\"Invalid user session. Please login to use service.\"}").build();
+		}
+		// Do logic
+		Message orgMsg = DBUtils.prepareOrgEmailMessage(false, getUserId());
+		if (orgMsg != null) {
+			List<String> mobileNumbers = DBUtils.getAssociatedCustomerEmails(getUserId());
+			emailHandler.deliverMessage(orgMsg, mobileNumbers, new Properties());
+		}
+		
+		Message smsMsg = DBUtils.prepareOrgSMSMessage(false, getUserId());
+		if(smsMsg!=null){
+			List<String> mobileNumbers =DBUtils.getAssociatedCustomerMobiles(getUserId());
+			smsHandler.deliverMessage(smsMsg, mobileNumbers, new Properties());
+		}
+		
+		
+		String result = "{\"response\":0,\"message\":\"message  sent successfully.\"}";
+		return Response.status(200).header("content-type", "application/json").entity(result).build();
+	}
+	
+	
+	@POST
+	@Path("/cancelmsg")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response cancelMessage(UserToken message) {
+
+		// Authenticating and Authorizing request
+		if (authorizeUser(message.getUsertoken()) != 200) {
+			return Response.status(403).entity("Invalid user session. Please login to use service.").build();
+		}
+		// Do logic
+
+		Message orgMsg = DBUtils.prepareOrgEmailMessage(true, getUserId());
+		if (orgMsg != null) {
+			List<String> mobileNumbers = DBUtils.getAssociatedCustomerEmails(getUserId());
+			emailHandler.deliverMessage(orgMsg, mobileNumbers, new Properties());
+		}
+		
+		Message smsMsg = DBUtils.prepareOrgSMSMessage(true, getUserId());
+		if(smsMsg!=null){
+			List<String> mobileNumbers =DBUtils.getAssociatedCustomerMobiles(getUserId());
+			smsHandler.deliverMessage(smsMsg, mobileNumbers, new Properties());
+		}
+		String result = "{\"response\":0,\"message\":\"cancel message  sent successfully.\"}";
+		return Response.status(200).header("content-type", "application/json").entity(result).build();
+	}
+	
 
 	@POST
 	@Path("/logout")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response logout(String usertoken) {
-
-		if (usertoken != null) {
-
+	public Response logout(UserToken userToken) {
+		if (userToken != null && userToken.getUsertoken()!=null) {
 			// authentication logic
 			doLogout();
 			return Response.status(200).header("content-type", "application/json")
 					.entity("{\"message\":\"Successfully logged out.\"}").build();
-			
 		} else {
-			return Response.status(404).entity("Invalid session.").build();
+			return Response.status(404).header("content-type", "application/json").entity("{\"message\":\"Invalid session.\"}").build();
 		}
 	}
 
